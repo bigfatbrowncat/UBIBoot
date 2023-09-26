d9be8c61d2cadb054bf8bb0245d1fee48958dad6
Ilya Mizus <bigfatbrowncat@gmail.com>
2023-09-26 16:39:27 +0300

    Trying 480i option

--- src/main.c
+++ src/main.c
@@ -63,7 +63,7 @@ static char *kernel_params[] = {
 	[PARAM_ROOTTYPE] = "",
 	[PARAM_ROOTWAIT] = "rootwait",
 	[PARAM_LOGO] = "",
-	[PARAM_VIDEO] = "video=DPI-1:320x240",
+	[PARAM_VIDEO] = "video=DPI-1:320x480i",
 	[PARAM_QUIET] = "quiet",
 	[PARAM_NOCURSOR_1] = "vt.global_cursor_default=0",
 	[PARAM_NOCURSOR_2] = "vt.cur_default=1",
